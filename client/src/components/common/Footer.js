835fa7c875748064083014b9537960996ea36632
Omkar Khade <omkarkhade76@gmail.com>
2024-04-03 15:26:10 +0530

    MERN CODE

---
+++ client/src/components/common/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+	const link = "https://henok.us";
+	const target = "_blank";
+
+	return (
+		<center>
+		<div className="container">
+			Copyright © <small>{new Date().getFullYear()}</small> Omkar Khade{" "}
+		
+		</div>
+		</center>
+	);
+};
+
+export default Footer;
